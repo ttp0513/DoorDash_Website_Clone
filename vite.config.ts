c1f0e72baf449a482224a285959a55dfcf6b3f0b
ttp0513 <johnphanthe@gmail.com>
2026-01-16 16:28:06 -0500

    add base vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/DoorDash_Website_Clone",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
